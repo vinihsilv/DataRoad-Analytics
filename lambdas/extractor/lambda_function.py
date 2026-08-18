a9b7bb0119678a70977c5c0ed5ed6e1204dffa3f
vinihsilv <168356724+vinihsilv@users.noreply.github.com>
2026-08-17 21:33:50 -0300

    Refactor lambda_handler to integrate S3 data extraction functionality

--- lambdas/extractor/lambda_function.py
+++ lambdas/extractor/lambda_function.py
@@ -1,11 +1,17 @@
+import boto3
+
+
+from packages.dataroad_extractor_s3_client import DataroadExtractorS3Client
+from packages.dataroad_extractor_s3_enviroment import DataroadExtractorS3Environment
+from packages.dataroad_extractor_s3_enviroment import DataroadExtractorS3Environment
 
 
 def lambda_handler(event=None, context=None):
-    print("Lambda invoked")
-    return {
-        "statusCode": 200,
-        "body": "Hello from local lambda"
-    }
+    print("Starting extractor lambda function...")
+    client_s3 = DataroadExtractorS3Client(s3_client=boto3.client("s3"))
+
+    print("Getting csv file from S3 bucket...")
+    client_s3.get_object(bucket_name="my-bucket", object_key="data.csv")
 
 
 if __name__ == "__main__":
